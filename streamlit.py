32e8adc136e32efa4453b0e6bbf34b892038d3d9
ansellum <jbvogt@ucdavis.edu>
2023-11-28 15:33:54 -0800

    Changed streamlit to use rbf

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,4 @@
-from sklearn.neural_network import MLPClassifier
+from sklearn import svm
 import pandas as pd
 from pandas import read_csv
 from sklearn.model_selection import train_test_split
@@ -104,10 +104,10 @@ def user_input_features():
     return features
     
     
-df = user_input_features()
+user_input = user_input_features()
 
 st.subheader('User Input Parameters')
-st.write(df)
+st.write(user_input)
 
 # Function to prepare the new inputs
 def prepare_new_input(df,oe,scaler):
@@ -119,53 +119,52 @@ def prepare_new_input(df,oe,scaler):
 
 # load the dataset
 def load_dataset(filename, column):
-    # load the dataset as a pandas DataFrame
-    df = read_csv(filename)
-    # Drop the 'veil_type' column
-    df = df.drop('veil-type', axis=1)
-    # split into input (X) and output (y) variables & convert to numPy array
-    X = df.drop(column, axis = 1).values
-    y = df[column].values
-    return X, y
+	# load the dataset as a pandas DataFrame
+	df = read_csv(filename)
+	df_shuffled = df.sample(frac=1)
+	# split into input (X) and output (y) variables & convert to numPy array
+	X = df_shuffled.drop(column, axis = 1)
+	y = df_shuffled[column]
+	return X, y
 
 # prepare input data
-def prepare_inputs(X_train, X_test):
-    oe = OrdinalEncoder()
-    oe.fit(X_train)
-    X_train_enc = oe.transform(X_train)
-    X_test_enc = oe.transform(X_test)
-        
-    # scale dataset
-    scaler = preprocessing.MinMaxScaler()
-    X_train_rescaled = scaler.fit_transform(X_train_enc)
-    X_test_rescaled = scaler.fit_transform(X_test_enc)
-    return X_train_rescaled, X_test_rescaled, oe, scaler
+def prepare_inputs(X):
+	oe = OrdinalEncoder()
+	oe.fit(X)
+	X_enc = oe.transform(X)
+		
+	# scale dataset
+	scaler = preprocessing.MinMaxScaler()
+	X_enc_scaled = scaler.fit_transform(X_enc)
+	return X_enc_scaled, oe, scaler
 
 # prepare target
-def prepare_targets(y_train, y_test):
-    le = LabelEncoder()
-    le.fit(y_train)
-    y_train_enc = le.transform(y_train)
-    y_test_enc = le.transform(y_test)
-    return y_train_enc, y_test_enc
+def prepare_targets(y):
+	le = LabelEncoder()
+	le.fit(y)
+	y_enc = le.transform(y)
+	return y_enc
 
 # load the dataset
 X, y = load_dataset('mushrooms.csv', 'class')
-# split into train and test sets; 80/20 split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+X = X.drop('veil-type', axis = 1)
+
 # prepare input data
-X_train_enc, X_test_enc, xoe, scaler = prepare_inputs(X_train, X_test)
+X_enc, xoe, xscaler = prepare_inputs(X)
 # prepare output data
-y_train_enc, y_test_enc = prepare_targets(y_train, y_test)
+y_enc = prepare_targets(y)
 
-mlp = MLPClassifier(solver = 'adam', random_state = 42, activation = 'logistic', learning_rate_init = 0.1, batch_size = 100, hidden_layer_sizes = (100,), max_iter = 200)
+# split into train and test sets; 80/20 split
+X_train_enc, X_test_enc, y_train_enc, y_test_enc = train_test_split(X_enc, y_enc, test_size=0.2, random_state=1)
 
-mlp.fit(X_train_enc, y_train_enc)
+rbf = svm.SVC(kernel='rbf', gamma=0.5).fit(X_train_enc, y_train_enc)
+
+rbf.fit(X_train_enc, y_train_enc)
 # pred = mlp.predict(X_test_enc)
 
-input_pre = prepare_new_input(df,xoe, scaler)
-pred = mlp.predict(input_pre)
-pred_proba = mlp.predict(input_pre)
+input_pre = prepare_new_input(user_input, xoe, xscaler)
+pred = rbf.predict(input_pre)
+pred_proba = rbf.predict(input_pre)
 
 # st.subleader('Class labels and their corresponding index number')
 # st.write(df2.target_names)
